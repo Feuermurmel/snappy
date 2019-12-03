23a3e2ee9a0064d098f87affcb0fe219f07089d0
Michael Schwarz <michi.schwarz@gmail.com>
2019-12-03 14:54:19 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name='snappy',
+    version='1.0.0',
+    packages=['snappy'],
+    entry_points=dict(
+        console_scripts=[]),
+    install_requires=[])
